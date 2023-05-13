186538f6bc78022d4c74ea7b39d12c94ee6be45c
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2023-05-13 11:36:40 +0600

    Font Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-mont`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
